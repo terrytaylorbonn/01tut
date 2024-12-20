ea730072a5201faebfe179af912af96c1339757f
T Taylor <terrytaylorbonn@gmail.com>
2024-12-20 13:56:29 -0500

    branch4

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,44 @@
 //Content.js
 import React from 'react'
 import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
-  const [name, setName] = useState('name1');
-  const [count, setCount] = useState(0);
-  const handleNameChange = () => {
-        const names = ['name1', 'name2', 'name3'];
-        const init = Math.floor(Math.random() * 3);
-        return setName(names[init]);
-  }
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  }
-  const handleClick2 = (name) => {
-    console.log(count);
-  }
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  }
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: 'Item 1',
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2',
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Item 3',
+    }
+  ]);
+
+
   return (
     <main>
-      <p onDoubleClick={handleClick } >hello {name}</p>
-      <button onClick={handleNameChange}>Change namee</button>
-      <button onClick={handleClick}>click me</button>
-      <button onClick={handleClick2}>click me</button>
+      <ul> 
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt
+              role="button"
+              tabIndex="0" />
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
